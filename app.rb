6631db8d8d0ab98542873bb8b3a30826752c9345
pacosta613 <pedro.acosta613@gmail.com>
2016-02-11 15:32:06 -0500

    Done.

--- app.rb
+++ app.rb
@@ -7,7 +7,6 @@ class App < Sinatra::Base
   end
 
   post '/food' do
-    "My name is #{params[:name]}, and I love #{params[:favorite_food]}"
+    "My name is #{params[:name].to_s}, and I love #{params[:favorite_food].to_s}"
   end
-
 end
