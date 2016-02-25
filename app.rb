781f2101e35f89ba7ac62f902c1d607ca025097e
Smulligan85 <sean.mulligan85@gmail.com>
2016-02-25 07:40:49 -0800

    Done

--- app.rb
+++ app.rb
@@ -1,3 +1,4 @@
+require 'pry'
 require_relative 'config/environment'
 
 class App < Sinatra::Base
@@ -7,6 +8,7 @@ class App < Sinatra::Base
   end
 
   # Add your post route and action below
-
-
+  post '/food' do
+    "My name is #{params[:name]}, and I love #{params[:favorite_food]}"
+  end
 end
