295708bbee4243ec4435405396545863f3926a07
kirangp <kirangp@gmail.com>
2016-05-12 23:25:44 +1000

    Able to connect HTML to Sinatra

--- app.rb
+++ app.rb
@@ -1,12 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  params = {
+  :name => "Carl",
+  :favorite_food => "fried chicken"
+}
 
   get '/food_form' do
     erb :food_form
   end
 
   # Add your post route and action below
+  post '/food' do
+    "My name is #{params[:name]}, and I love #{params[:favorite_food]}"
+  end
 
 
 end
