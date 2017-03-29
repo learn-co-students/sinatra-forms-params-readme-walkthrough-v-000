36ee29e2c80e47c2a2fe0ab491674c9fbe328844
Daniel Lempesis <danlrcn@hotmail.com>
2017-03-29 17:40:28 +0000

    Done.

--- app.rb
+++ app.rb
@@ -8,5 +8,8 @@ class App < Sinatra::Base
 
   # Add your post route and action below
 
-
+  post '/food' do #NOTICE this is POST not GET. Get returns a fuckton of html :/
+    #"Hi, #{params[:name]}! You've been redirected! Enjoy some #{params[:favorite_food]}."
+    "My name is #{params[:name]}, and I love #{params[:favorite_food]}"
+  end
 end
