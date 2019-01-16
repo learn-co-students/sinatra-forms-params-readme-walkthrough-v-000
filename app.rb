e0c48eb4d6e355fa49cecd43295c184149c064df
Benito Lorenzo Santos <benitolsantos@gmail.com>
2019-01-16 20:17:20 +0000

    Done.

--- app.rb
+++ app.rb
@@ -7,5 +7,12 @@ class App < Sinatra::Base
   end
 
   # Add your post route and action below
-
+  
+  #Every form also needs a corresponding route in the controller file (app.rb).
+  #Instead of a get route (which we used to route users to view an html page),
+  #we'll set up a post route:
+  post '/food' do
+    #params.to_s
+    "My name is #{params[:name]}, and I love #{params[:favorite_food]}"
+  end
 end
