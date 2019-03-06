40073579be6e0aa4a5f5eb409a87bac97973f245
Cheryl K <cherylkelson@icloud.com>
2019-03-06 18:36:51 +0000

    Done.

--- app.rb
+++ app.rb
@@ -3,13 +3,7 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/food_form' do
-    #fill_in("name", with: agent)
-    # params = {
-    #   :name => "agent",
-    #   favorite_food => "Green Eggs and Ham"
-    # }
     erb :food_form
-
   end
 
 
@@ -18,10 +12,5 @@ class App < Sinatra::Base
   post '/food' do
     params.to_s
     "My name is #{params[:name]}, and I love #{params[:favorite_food]}"
-    # erb :food_form 
-  #   params = {
-  #   :name => "Sam",
-  #   :favorite_food => "Green Eggs and Ham"
-  # }
       end
   end
