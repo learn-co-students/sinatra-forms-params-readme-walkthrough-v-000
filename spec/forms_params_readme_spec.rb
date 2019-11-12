2f7acacfad805f2441bec85b1c097b25d5e4b976
Brandon Brasson <rocksong622@gmail.com>
2019-11-12 17:17:08 +0000

    Done.

--- spec/forms_params_readme_spec.rb
+++ spec/forms_params_readme_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-puts describe "App" do
+ describe "App" do
   describe 'GET /food_form' do
     it "can fill in the form" do
       agent = "Carl Jr."
@@ -16,7 +16,7 @@ puts describe "App" do
     end
   end
 
-  puts describe 'POST /food' do
+   describe 'POST /food' do
     it 'responds with a 200' do
       post '/food'
 
